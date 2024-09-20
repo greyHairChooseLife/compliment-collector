239a7d384c3823a364f3ec71b3c50ee2869bd920
sangyeon kim <greyhairchooselife@gmail.com>
2024-09-21 06:09:09 +0900

    perf!: 람다 호출을 최소화하기 위해 expires time을 활용한 캐싱

--- src/api/authFromLambda.ts
+++ src/api/authFromLambda.ts
@@ -1,4 +1,4 @@
-export const getGoogleAuthToken = async () => {
+export const getGoogleAuthData = async () => {
   const response = await fetch(
     'https://w0s4wu14e9.execute-api.us-east-1.amazonaws.com/default/compliment-collector',
     {
@@ -6,6 +6,5 @@ export const getGoogleAuthToken = async () => {
     },
   );
 
-  const data = await response.json();
-  return data.access_token; // 액세스 토큰을 반환
+  return await response.json();
 };
--- src/api/google-sheet.ts
+++ src/api/google-sheet.ts
@@ -1,11 +1,27 @@
 import { GoogleSpreadsheet } from 'google-spreadsheet';
-import { getGoogleAuthToken } from './authFromLambda';
+import { getGoogleAuthData } from './authFromLambda';
 
 const SHEET_ID = '1QtknLF9IMTYVPD-mika5K6a9P4-shE7kwPyyWkbBcPY';
 
-const getSheet = async () => {
-  const accessToken = await getGoogleAuthToken();
+let cachedToken: string | null = null;
+let tokenExpirationTime: number | null = null;
 
+const getGoogleAuthTokenCached = async () => {
+  const now = Date.now(); // 이것은 미리초 단위
+
+  if (!cachedToken || !tokenExpirationTime || tokenExpirationTime < now) {
+    const authData = await getGoogleAuthData();
+    cachedToken = authData.access_token;
+    tokenExpirationTime = now + authData.expires_in * 1000; // 람다에서 전달받은 만료시간은 초 단위이다.
+  }
+
+  return cachedToken;
+};
+
+const getSheet = async () => {
+  const accessToken = await getGoogleAuthTokenCached();
+
+  if (!accessToken) throw new Error('No access token');
   const doc = new GoogleSpreadsheet(SHEET_ID, { token: accessToken });
 
   return doc;
