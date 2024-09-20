dffbbff710344210f3927c7a1e99359629c413df
sangyeon kim <greyhairchooselife@gmail.com>
2024-09-21 07:10:59 +0900

    feat: 칭찬 작성란 각각 생성

--- src/App.tsx
+++ src/App.tsx
@@ -10,11 +10,14 @@ type Record = {
 function App() {
   const [leftRecords, setLeftRecords] = useState<Record[]>([]);
   const [rightRecords, setRightRecords] = useState<Record[]>([]);
+  const [reloadRecords, setReloadRecords] = useState(true);
+  const [leftNewPraise, setLeftNewPraise] = useState('');
+  const [rightNewPraise, setRightNewPraise] = useState('');
 
   useEffect(() => {
     const loadRows = async () => {
-      const leftRows = await getRows('YtoS');
-      const rightRows = await getRows('StoY');
+      const leftRows = await getRows('YtoS'); // 유진 -> 상연
+      const rightRows = await getRows('StoY'); // 상연 -> 유진
 
       const leftRecords = leftRows.map((row) => ({
         date: row.get('일자'),
@@ -28,37 +31,83 @@ function App() {
 
       setLeftRecords(leftRecords);
       setRightRecords(rightRecords);
+
+      setReloadRecords(false);
     };
 
-    // const addRow = async () => {
-    //   await addRows('YtoS', 'Hello');
-    // };
+    if (reloadRecords) loadRows();
+  }, [reloadRecords]);
 
-    loadRows();
-    // addRow();
-  }, []);
+  const addPraise = async (sheetName: string) => {
+    if (sheetName === 'YtoS') {
+      await addRows(sheetName, leftNewPraise);
+      setLeftNewPraise('');
+    } else {
+      await addRows(sheetName, rightNewPraise);
+      setRightNewPraise('');
+    }
+    setReloadRecords(true);
+  };
+
+  const onChangePrase = (
+    e: React.ChangeEvent<HTMLInputElement>,
+    sheetName: string,
+  ) => {
+    if (sheetName === 'YtoS') setLeftNewPraise(e.target.value);
+    else setRightNewPraise(e.target.value);
+  };
 
   return (
     <>
       <h2>오늘의 칭찬</h2>
+      <div>
+        <div>
+          <label>유진 작성: </label>
+          <input
+            type="text"
+            placeholder="칭찬을 입력하세요"
+            value={leftNewPraise}
+            onChange={(e) => onChangePrase(e, 'YtoS')}
+          />
+          <button onClick={() => addPraise('YtoS')}>업로드</button>
+        </div>
+        <div>
+          <label>상연 작성: </label>
+          <input
+            type="text"
+            placeholder="칭찬을 입력하세요"
+            value={rightNewPraise}
+            onChange={(e) => onChangePrase(e, 'StoY')}
+          />
+          <button onClick={() => addPraise('StoY')}>업로드</button>
+        </div>
+      </div>
       <h2>그간의 기록</h2>
       <div>
-        {leftRecords.map((record) => (
-          <div key={record.date + record.praise}>
-            <p>
-              {record.praise}
-              <i> _ {record.date}</i>
-            </p>
-          </div>
-        ))}
-        {rightRecords.map((record) => (
-          <div key={record.date + record.praise}>
-            <p>
-              {record.praise}
-              <i> _ {record.date}</i>
-            </p>
-          </div>
-        ))}
+        <div>
+          <h3>to 상연</h3>
+          {reloadRecords && <p>Loading...</p>}
+          {leftRecords.map((record) => (
+            <div key={record.date + record.praise}>
+              <p>
+                {record.praise}
+                <i> _ {record.date}</i>
+              </p>
+            </div>
+          ))}
+        </div>
+        <div>
+          <h3>to 유진</h3>
+          {reloadRecords && <p>Loading...</p>}
+          {rightRecords.map((record) => (
+            <div key={record.date + record.praise}>
+              <p>
+                {record.praise}
+                <i> _ {record.date}</i>
+              </p>
+            </div>
+          ))}
+        </div>
       </div>
     </>
   );
