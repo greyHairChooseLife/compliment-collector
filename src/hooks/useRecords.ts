c71124c85eac413e066d7b0fc1c8a45f5f1c6c5a
sangyeon kim <greyhairchooselife@gmail.com>
2024-11-08 04:38:08 +0900

    refactor: UI와 데이터 관리를 분리

---
+++ src/hooks/useRecords.ts
@@ -0,0 +1,102 @@
+import { useState, useEffect } from 'react';
+import { getRows, addRows } from '../api/google-sheet';
+import { isWritable } from '../features';
+import { Record, IsWritable } from '../types';
+
+const useRecords = () => {
+  const [leftRecords, setLeftRecords] = useState<Record[]>([]);
+  const [rightRecords, setRightRecords] = useState<Record[]>([]);
+  const [reloadRecords, setReloadRecords] = useState(true);
+  const [leftNewPraise, setLeftNewPraise] = useState('');
+  const [rightNewPraise, setRightNewPraise] = useState('');
+  const [areWritables, setAreWritables] = useState<IsWritable>({});
+
+  useEffect(() => {
+    const loadRows = async () => {
+      const leftRows = await getRows('YtoS'); // 유진 -> 상연
+      const rightRows = await getRows('StoY'); // 상연 -> 유진
+
+      const leftRecords = leftRows.map((row) => ({
+        origin_date: row.get('일자'),
+        local_date: row.get('한국기준시'),
+        show_date: row
+          .get('한국기준시')
+          .split(' ')
+          .map((v: string, i: number) =>
+            i === 0
+              ? v.substring(2).replace(/-/g, '/')
+              : v.slice(0, 2).concat('시'),
+          )
+          .join(' '),
+        praise: row.get('칭찬'),
+      }));
+
+      const rightRecords = rightRows.map((row) => ({
+        origin_date: row.get('일자'),
+        local_date: row.get('한국기준시'),
+        show_date: row
+          .get('한국기준시')
+          .split(' ')
+          .map((v: string, i: number) =>
+            i === 0
+              ? v.substring(2).replace(/-/g, '/')
+              : v.slice(0, 2).concat('시'),
+          )
+          .join(' '),
+        praise: row.get('칭찬'),
+      }));
+
+      setAreWritables({
+        leftPraise: isWritable({ records: leftRecords }),
+        rightPraise: isWritable({ records: rightRecords }),
+      });
+
+      setLeftRecords(leftRecords);
+      setRightRecords(rightRecords);
+
+      setReloadRecords(false);
+    };
+
+    if (reloadRecords) loadRows();
+  }, [reloadRecords]);
+
+  const addPraise = async (sheetName: string) => {
+    if (sheetName === 'YtoS') {
+      if (leftNewPraise === '') {
+        window.alert('칭찬을 입력하세요');
+        return;
+      }
+      await addRows(sheetName, leftNewPraise);
+      setLeftNewPraise('');
+    } else {
+      if (rightNewPraise === '') {
+        window.alert('칭찬을 입력하세요');
+        return;
+      }
+      await addRows(sheetName, rightNewPraise);
+      setRightNewPraise('');
+    }
+    setReloadRecords(true);
+  };
+
+  const onChangePrase = (
+    e: React.ChangeEvent<HTMLTextAreaElement>,
+    sheetName: string,
+  ) => {
+    if (sheetName === 'YtoS') setLeftNewPraise(e.target.value);
+    else setRightNewPraise(e.target.value);
+  };
+
+  return {
+    leftRecords,
+    rightRecords,
+    leftNewPraise,
+    rightNewPraise,
+    reloadRecords,
+    areWritables,
+    addPraise,
+    onChangePrase,
+  };
+};
+
+export default useRecords;
