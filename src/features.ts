933d5e10455b32d8bbaae16df793f062649326b0
sangyeon kim <greyhairchooselife@gmail.com>
2024-11-11 16:55:51 +0900

    typo:

--- src/features.ts
+++ src/features.ts
@@ -9,11 +9,11 @@ type isWritableProps = {
 export const isWritable = (props: isWritableProps) => {
   const { records } = props;
   //마지막 칭찬의 시간을 가져오기
-  const lastPraseTime = new Date(records[records.length - 1].local_date);
+  const lastPraiseTime = new Date(records[records.length - 1].local_date);
 
   //이것이 현지 시간 기준 '금일' 작성되었는지 확인
   const todayInit = new Date();
   todayInit.setHours(0, 0, 0, 0);
 
-  return lastPraseTime < todayInit;
+  return lastPraiseTime < todayInit;
 };
