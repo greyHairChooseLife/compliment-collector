06d15d9d9aee600ff643d7d25921f73f0c562ac4
sangyeon kim <greyhairchooselife@gmail.com>
2024-11-11 17:17:14 +0900

    fix: 예외처리, 작성된 내용이 하나도 없는 경우

--- src/features.ts
+++ src/features.ts
@@ -9,7 +9,12 @@ type isWritableProps = {
 export const isWritable = (props: isWritableProps) => {
   const { records } = props;
   //마지막 감사의 시간을 가져오기
-  const lastPraiseTime = new Date(records[records.length - 1].local_date);
+  const lastPraisLocalDate = records[records.length - 1]?.local_date;
+
+  //예외처리: 하나도 작성하지 않은 경우
+  if (!lastPraisLocalDate) return true;
+
+  const lastPraiseTime = new Date(lastPraisLocalDate);
 
   //이것이 현지 시간 기준 '금일' 작성되었는지 확인
   const todayInit = new Date();
